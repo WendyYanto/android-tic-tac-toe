736e8d2b8d8e8e952a20613d105032ea588c2d69
wendyyanto <yanto.wendy98@gmail.com>
2020-04-26 23:18:39 +0700

    Set view to have equal width and height

--- app/src/main/java/com/example/tictactoe/TicTacToeView.kt
+++ app/src/main/java/com/example/tictactoe/TicTacToeView.kt
@@ -6,4 +6,11 @@ import android.view.View
 
 class TicTacToeView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : View(context, attrs, defStyleAttr) 
+) : View(context, attrs, defStyleAttr) {
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val size = widthMeasureSpec.coerceAtMost(heightMeasureSpec)
+        setMeasuredDimension(size, size)
+    }
+}
